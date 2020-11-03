63a9a27f2f0cd84929026eb9aaea0a104a3d4c8f
Wataru-Toriumi <wataru.19940213@gmail.com>
2020-11-03 11:22:55 +0900

    [add] パッケージの追加

--- pkgsample/mdlsample.py
+++ pkgsample/mdlsample.py
@@ -1,6 +1,7 @@
+import requests
+
+def get(url='https://www.google.com/?hl=ja'):
+    return requests.get(url)
+
 def func_sample(x, y, z):
     return x + y + z
-
-
-if __name__ == '__main__':
-    func_sample(1, 2, 3)
